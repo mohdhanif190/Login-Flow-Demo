93bdfdae6838e26b718bd51bc2abd06cc73f4e7c
mohdhanif190 <mohdhanif190@gmail.com>
2024-09-01 20:27:52 -0400

    Initial Commit

---
+++ Login Flow Demo/Login_Flow_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_Flow_DemoApp.swift
+//  Login Flow Demo
+//
+//  Created by MohdHanif Bhatkar on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Login_Flow_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
